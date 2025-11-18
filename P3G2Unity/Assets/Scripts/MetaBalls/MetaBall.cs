fae28bd88bccfb9626d19af7ddec8cc77a0f48bb
oskaroskaroskaroskaroskar <ZQ21JJ@student.aau.dk>
2025-11-18 13:16:12 +0100

    Stuff

---
+++ P3G2Unity/Assets/Scripts/MetaBalls/MetaBall.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MetaBall : MonoBehaviour {
+    protected float radius;
+    public bool negativeBall;
+
+    public float factor;
+    private void Awake()
+    {
+        radius = transform.localScale.x;
+    }
+    public virtual void Start() {
+        this.factor = (this.negativeBall ? -1 : 1) * this.radius * this.radius * 2;
+    }
+}
