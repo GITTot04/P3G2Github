a366fabb46a172fb9f7651e0ef47b22be80cb071
GITTot04 <112081156+GITTot04@users.noreply.github.com>
2025-11-21 14:42:05 +0100

    metas

--- P3G2Unity/Assets/Scripts/ColourChoosing.cs
+++ P3G2Unity/Assets/Scripts/ColourChoosing.cs
@@ -1,12 +1,21 @@
 using UnityEngine;
 using System.Collections;
+using TMPro;
+using UnityEngine.UI;
+//using UnityEngine.UIElements;
 
 public class ColourChoosing : MonoBehaviour
 {
     GameObject colourMenu;
+    public GameObject resetCircle;
+    public GameObject resetCircle1;
+    float resetFillAmount;
+    float timeToReset = 5f;
+    float resetHeldTime;
     public Test handController;
     public Color drawingColour = Color.gray;
     public Transform mainCameraTransform;
+    public LayerMask layerMaskUI;
 
     private void Awake()
     {
@@ -19,6 +28,7 @@ public class ColourChoosing : MonoBehaviour
         {
             colourMenu.transform.localPosition = new Vector3(-2, 0, 0);
         }
+        layerMaskUI = LayerMask.GetMask("UI");
     }
 
     public IEnumerator PickingColours()
@@ -27,8 +37,8 @@ public class ColourChoosing : MonoBehaviour
         {
             RaycastHit hit;
             Vector3 rayDirection = handController.drawingHandTracker.transform.position - mainCameraTransform.position;
-            Ray ray = new Ray(handController.drawingHandTracker.transform.position, rayDirection);
-            Physics.Raycast(ray, out hit);
+            Ray ray = new Ray(mainCameraTransform.position, rayDirection);
+            Physics.Raycast(ray, out hit, layerMaskUI);
             if (hit.collider != null)
             {
                 switch (hit.collider.gameObject.name)
@@ -54,11 +64,33 @@ public class ColourChoosing : MonoBehaviour
                     case "Magenta":
                         drawingColour = Color.magenta;
                         break;
+                    case "Reset":
+                        resetHeldTime += Time.deltaTime;
+                        resetFillAmount = resetHeldTime / timeToReset;
+                        resetCircle.GetComponent<Image>().fillAmount = resetFillAmount;
+                        resetCircle1.GetComponent<Image>().fillAmount = resetFillAmount;
+                        if (resetFillAmount >= 1)
+                        {
+                            ResetDrawing();
+                        }
+                        break;
                     default:
                         break;
                 }
             }
+            else
+            {
+                resetHeldTime = 0;
+                resetCircle.GetComponent<Image>().fillAmount = 0;
+                resetCircle1.GetComponent<Image>().fillAmount = 0;
+            }
             yield return null;
         }
+    }
+    public void ResetDrawing()
+    {
+        resetHeldTime = 0;
+        resetCircle.GetComponent<Image>().fillAmount = 0;
+        resetCircle1.GetComponent<Image>().fillAmount = 0;
     }
 }
